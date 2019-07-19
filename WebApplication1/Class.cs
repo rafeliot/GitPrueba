16a530a93ebc8af66e05644d5b6db0e6afda7f38
Rafeliot <rafeliot@gmail.com>
2019-07-19 15:32:04 -0400

    add

--- WebApplication1/Class.cs
+++ WebApplication1/Class.cs
@@ -7,5 +7,12 @@ namespace WebApplication1
 {
     public class Class
     {
+        public int add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
